6ab47acf7c27c69a974b80d310e362fe35a72fa2
李云辉 <2921990017@qq.com>
2021-01-28 15:36:46 +0800

    12

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("来了");
         System.out.println("sss");
-        System.out.println("ppp");
     }
 
 }
